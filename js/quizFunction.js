489c44d8f85ddfd4a0351a3a69ca2354ba42379a
dipalibaraskar <dipalibaraskar77@gmail.com>
2023-11-05 19:37:33 -0500

    MCQ code change js file

--- js/quizFunction.js
+++ js/quizFunction.js
@@ -1,67 +1,69 @@
-var count = 0;
-var time = 30;
-var marks = 0;
-var answer = [];
-var timer;
 
-var myData = {
-    "Questions": [
-        {
-            "Quiz": "Q.1) What does HTML stand for?",
-            "option1": "Hyper Type Markup Language",
-            "option2": "Home Tool Markup Language",
-            "option3": "Hyper Link and Text Markup Language",
-            "option4": "Hyper Text Markup Language",
-            "answer": "Hyper Text Markup Language"
-
-        },
-        {
-            "Quiz": "Q.2) What does CSS stand for?",
-            "option1": "Colorful Style sheets",
-            "option2": "Computer Style Sheets",
-            "option3": "Creative STyle Sheets",
-            "option4": "Cascading Style Sheets",
-            "answer": "Cascading Style Sheets"
-
-        },
-        {
-            "Quiz": "Q.3) Which HTML attribute is used to define inline styles?",
-            "option1": "Style",
-            "option2": "Font",
-            "option3": "Class",
-            "option4": "Styles",
-            "answer": "Style"
-
-        },
-        {
-            "Quiz": "Q.4) Which is the correct CSS syntax?",
-            "option1": "body:color=black;",
-            "option2": "{body:color=black;}",
-            "option3": "body{color:black;}",
-            "option4": "{body;color:black;}",
-            "answer": "body{color:black;}"
-
-        },
-        {
-            "Quiz": "Q.5) Which is the correct HTML tag for playing video files?",
-            "option1": "media",
-            "option2": "movie",
-            "option3": "video",
-            "option4": "mp4",
-            "answer": "video"
-
-        }
-
-    ]
-};
 //main ready function
 $(document).ready(function () {
-    
+
+    var count = 0;
+    var time = 30;
+    var marks = 0;
+    var answer = [];
+    var timer;
+
+    var myData = {
+        "Questions": [
+            {
+                "Quiz": "Q.1) What does HTML stand for?",
+                "option1": "Hyper Type Markup Language",
+                "option2": "Home Tool Markup Language",
+                "option3": "Hyper Link and Text Markup Language",
+                "option4": "Hyper Text Markup Language",
+                "answer": "Hyper Text Markup Language"
+
+            },
+            {
+                "Quiz": "Q.2) What does CSS stand for?",
+                "option1": "Colorful Style sheets",
+                "option2": "Computer Style Sheets",
+                "option3": "Creative STyle Sheets",
+                "option4": "Cascading Style Sheets",
+                "answer": "Cascading Style Sheets"
+
+            },
+            {
+                "Quiz": "Q.3) Which HTML attribute is used to define inline styles?",
+                "option1": "Style",
+                "option2": "Font",
+                "option3": "Class",
+                "option4": "Styles",
+                "answer": "Style"
+
+            },
+            {
+                "Quiz": "Q.4) Which is the correct CSS syntax?",
+                "option1": "body:color=black;",
+                "option2": "{body:color=black;}",
+                "option3": "body{color:black;}",
+                "option4": "{body;color:black;}",
+                "answer": "body{color:black;}"
+
+            },
+            {
+                "Quiz": "Q.5) Which is the correct HTML tag for playing video files?",
+                "option1": "media",
+                "option2": "movie",
+                "option3": "video",
+                "option4": "mp4",
+                "answer": "video"
+
+            }
+
+        ]
+    };
+
     // hide elements initially
     $("#mainBody, #finish, #Result").hide();
     $("#rightAns, #wrongAns, #congrats").hide();
     $(".option").removeAttr('disabled');
-    
+
     buttons_manager();
     //create function
     function buttons_manager() {
@@ -116,13 +118,13 @@ $(document).ready(function () {
         $("#marks").text(marks);
         $("#correct-answer").text(marks / 5);
         //$('#correct-answer').text((marks / 25) * 100 + "%");
-        
+
         $("#percentage").text((marks / 25) * 100 + "%");
         $("#Result").show();
         if ((marks / 5) >= 2) {
             $("#congrats, #trophy").show();
         }
-        else{
+        else {
             $("#congrats, #trophy").hide();
         }
     }
@@ -174,19 +176,18 @@ $(document).ready(function () {
     //on button click start
 
     $('#btn').click(function () {
-       
+
 
         //alert("button clicked");
         $('#options, #mainBody').show();
         adding_Questions(myData.Questions, count);
         $('.start_page').hide();
         $('#prev').hide();
-        $("#startQuiz").css("display","none");
+        $("#startQuiz").css("display", "none");
 
         timer = setInterval(timer_function, 1000);
 
-        function timer_function()
-        {
+        function timer_function() {
             $('#time').text(time);
             if (time < 1) {
                 clearInterval(timer);
@@ -197,7 +198,7 @@ $(document).ready(function () {
 
             }
             time--;
-        } 
+        }
 
     });
 
@@ -206,16 +207,16 @@ $(document).ready(function () {
 
         $(this).addClass("active");
         $(this).siblings().removeClass("active");
-        $(this).siblings().attr('disabled','disabled');
+        $(this).siblings().attr('disabled', 'disabled');
         // answer[count] = $(this).html();
         let currentAnswer = $(this).html();
-        
-        if(myData.Questions[count].answer == currentAnswer){
+
+        if (myData.Questions[count].answer == currentAnswer) {
             answer[count] = currentAnswer;
             $("#rightAns").show();
             $("#wrongAns").hide();
         }
-        else{
+        else {
             answer[count] = 0;
             $("#wrongAns").show();
             $("#rightAns").hide();
@@ -268,7 +269,7 @@ $(document).ready(function () {
         }
         else {
             creating_result(myData);
-            clearInterval(timer); 
+            clearInterval(timer);
         }
     });
 
